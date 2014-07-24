a7060639c08e9c2841eea0f94eb409d8b63f832e
Kyle Berger <bergerk@atmbergek01.atdom.ad.agilex.com>
2014-07-24 14:05:05 -0400

    changed index page
    yay

---
+++ js/scrolling.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(window).on('scroll',function(){
+      // we round here to reduce a little workload
+      stop = Math.round($(window).scrollTop());
+      if (stop < 200) {//top
+        $('.navbar-inverse').removeAttr("style");
+        $('.navbar-brand').removeAttr("style");
+        $('.navbar-inverse .navbar-nav>li>a').removeAttr("style");
+      } else {//bottom
+        $('.navbar-inverse').css({'background-color': 'white', 'opacity': '.9', 'box-shadow' : '0 8px 8px -4px rgba(0,0,0,0.3)'});
+        $('.navbar-brand').css('color', '#000');
+        $('.navbar-inverse .navbar-nav>li>a').css('color', '#000');
+      }
+  });
+});
