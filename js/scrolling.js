d2d00097de7f97ef81d29e77c5a8cce6b413d1a7
Kyle Berger <bergerk@atmbergek01.atdom.ad.agilex.com>
2014-07-24 14:31:16 -0400

    fixed issue
    bug

--- js/scrolling.js
+++ js/scrolling.js
@@ -6,10 +6,14 @@ $(document).ready(function(){
         $('.navbar-inverse').removeAttr("style");
         $('.navbar-brand').removeAttr("style");
         $('.navbar-inverse .navbar-nav>li>a').removeAttr("style");
+        $('.icon-bar').removeAttr("style");
+        $('.navbar-toggle:hover').removeAttr("style");
       } else {//bottom
         $('.navbar-inverse').css({'background-color': 'white', 'opacity': '.9', 'box-shadow' : '0 8px 8px -4px rgba(0,0,0,0.3)'});
         $('.navbar-brand').css('color', '#000');
         $('.navbar-inverse .navbar-nav>li>a').css('color', '#000');
+        $('.icon-bar').css('background-color', '#000');
+        $('.navbar-toggle').css('background-color', 'transparent');
       }
   });
 });
